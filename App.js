a9552fc30f335a206aa6c06e9c6dcffb9d13c046
azizkhuja <azeez@azeezs-macbook-pro.home>
2021-01-02 23:08:24 +0100

    added

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  StyleSheet,
+} from 'react-native';
+import Onboard from './components/Onboard';
+import Home from './components/Home';
+
+const App = () => {
+  const [showOnboard, setShowOnboard] = useState(true);
+
+  const handleOnboardFinish = () => {
+    setShowOnboard(false);
+  };
+
+  return (
+    <>
+      {showOnboard && <Onboard handleDone={handleOnboardFinish} />}
+      {!showOnboard && <Home />}
+    </>
+  );
+};
+
+export default App;
